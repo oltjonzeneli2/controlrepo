94eaa13f73fcaecfee1e20b49467f2064e4995fa
oltjonzeneli2 <63400163+oltjonzeneli2@users.noreply.github.com>
2020-04-09 18:13:56 +0200

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,3 +1,10 @@
 node default {
-  
+  file {'/root/README':
+    ensure => file,
+    content => 'This is first test file',
+  }
 }
+node 'master2.puppet.vm'{
+        include role::master_server
+        }
+
