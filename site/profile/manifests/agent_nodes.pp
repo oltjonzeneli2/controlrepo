823ae4b9ffea3ba8a576d43900de508c01c8dd11
oltjonzeneli2 <63400163+oltjonzeneli2@users.noreply.github.com>
2020-04-10 20:16:13 +0200

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -4,4 +4,16 @@
        dockeragent::node {'web1.puppet.vm':}
  	 dockeragent::node {'db.puppet.vm':}
        
-      }
+      host {'web.puppet.vm':
+            ensure => present,
+            ip => '172.18.0.2',
+            }
+      host {'web1.puppet.vm':
+            ensure => present,
+            ip => '172.18.0.4',
+            }
+      host {'db.puppet.vm':
+            ensure => present,
+            ip => '172.18.0.3',
+            }     
+     }
